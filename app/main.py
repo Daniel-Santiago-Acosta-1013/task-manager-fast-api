09b8c03d1477f6ecd3d29398fea9f4d783871677
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-05-09 23:52:28 -0500

    [add] terminando de configurar a main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from app.db.base import Base, engine
 from app.api.routes import users, projects, tasks
 
 app = FastAPI()
 
+# Create database tables
+Base.metadata.create_all(bind=engine)
+
+# Include API routers
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(projects.router, prefix="/projects", tags=["projects"])
 app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Task Manager API!"}
